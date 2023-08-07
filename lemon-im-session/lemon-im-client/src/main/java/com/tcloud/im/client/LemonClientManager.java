80fbe22c42f92bff8ff5d347c132859129bbd7cc
YBin <evans6183@163.com>
2023-08-07 17:09:20 +0800

    new Module

---
+++ lemon-im-session/lemon-im-client/src/main/java/com/tcloud/im/client/LemonClientManager.java
@@ -0,0 +1,4 @@
+package com.tcloud.im.client;
+
+public class LemonClientManager {
+}
--- lemon-im-session/lemon-im-websocket/src/main/java/com/example/im/websocket/LemonImWebsocketApplication.java
+++ lemon-im-session/lemon-im-websocket/src/main/java/com/example/im/websocket/LemonImWebsocketApplication.java
@@ -1,15 +1,13 @@
-package com.tcloud.im.client;
+package com.example.im.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Anker
- */
 @SpringBootApplication
-public class LemonImClientApplication {
+public class LemonImWebsocketApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(LemonImClientApplication.class, args);
+        SpringApplication.run(LemonImWebsocketApplication.class, args);
     }
 
 }
